4be137cfa7e2ae9d3479b6c19f2ba3e8641ff6d6
EstherOkiri <est.okiri@gmail.com>
2024-02-17 13:06:56 -0800

    add script.js file

---
+++ script.js
@@ -0,0 +1,12 @@
+let message = "Hello Zindua";
+let num1 = 3;
+let num2 = 2;
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let shopping = ['bread', 'milk', 'sugar','peanut'];
+
+console.log("Answer " + sum);
+console.log("Answer " + sub);
+console.log("Answer " + mult);
+console.log(shopping);
