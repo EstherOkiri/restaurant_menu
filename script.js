1c3581977f40d443e89311709ff88cae8cf73dc5
EstherOkiri <est.okiri@gmail.com>
2024-02-17 13:17:39 -0800

    add concatenation logic

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ let sum = num1 + num2;
 let sub = num1 - num2;
 let mult = num1 * num2;
 let shopping = ['bread', 'milk', 'sugar','peanut'];
+let name = "Esther";
+let newMessage = `${message}, my name is ${name}.`;
 
 console.log("Answer " + sum);
 console.log("Answer " + sub);
 console.log("Answer " + mult);
 console.log(shopping);
+console.log(newMessage);
